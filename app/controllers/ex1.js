37122c2a14bbcdd4b0af4efd2de44cb90baef6e8
Mathieu <mathieu.qfl@gmail.com>
2021-01-26 15:48:52 +0100

    fonction des boutons

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -10,5 +10,32 @@ export default class Ex1Controller extends Controller {
     return this.MAX - this.content.length;
   }
 
+get style(){
+  if(this.content.length > 0){
+    return 'alert alert-info';
+  }else{
+    return '';
+  }
+}
+
+@action
+update(){
+  if(this.content.length > 0){
+    this.info = 'Note modifiée';
+  }else{
+    this.info = '';
+  }
+}
+
+@action
+save(){
+  if(this.content.length > 0){
+    this.info = 'Note sauvegardée';
+    this.style = 'alert-success';
+    this.messageNote = this.content.toString();
+  }else{
+    this.info = '';
+  }
+}
 
 }
