6217c7ce0f4f5312902db7fd1a681e2a2fd06ddd
Mathieu <mathieu.qfl@gmail.com>
2021-01-26 15:16:06 +0100

    tp en cours

---
+++ app/controllers/ex1.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class Ex1Controller extends Controller {
+  @tracked content='';
+  MAX=100;
+
+  get size() {
+    return this.MAX - this.content.length;
+  }
+
+
+}
