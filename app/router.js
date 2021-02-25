0fc653f0d69d56d3b511dc6c3b2c67c757755a2d
Mathieu <mathieu.qfl@gmail.com>
2021-02-25 11:24:04 +0100

    conception des routes

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('categories', function() {
+    this.route('add');
+    this.route('update');
+  });
+});
